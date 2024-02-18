864e986ee1cf880f84f0a3c08ff80bfc78212a78
sumaiye <sumaiyeaakterbely@gmail.com>
2024-02-18 23:53:29 +0600

    first commit

---
+++ images/button.js
@@ -0,0 +1,5 @@
+
+    const button = document.getElementById('buuton');
+    button.addEventListener("click", function(){
+        button.style.backgroundColor='green'
+    })
---
+++ images/style.js
@@ -0,0 +1,9 @@
+function buynow(){
+   const playmain = document.getElementById('main');
+   playmain.classList.remove('hidden');
+}
+
+// function bacgcoleradd ( element){
+// const element = document.getElementById(element);
+// element.classList.add('bg-orange-400')
+// }
